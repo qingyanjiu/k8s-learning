3b04f8b39639d974bf3bf73832c1cd2518df6113
louisliu <louisliu@cisco.com>
2019-01-11 14:57:51 +0800

    added kubeless java test class

---
+++ kubeleess/src/main/java/kubeless/io/Query.java
@@ -0,0 +1,85 @@
+package kubeless.io;
+
+import io.kubeless.Event;
+import io.kubeless.Context;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Query {
+
+    private String driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://config.mokulive.stream:3306/storage?useUnicode=true&characterEncoding=utf-8&useSSL=false";
+    private String username = "root";
+    private String password = "19831226lc";
+
+    private static Connection conn = null;
+
+
+    public Map getData(io.kubeless.Event event, io.kubeless.Context context) {
+        System.out.println(event.Data);
+        Query query = new Query();
+        if("qryAllTypes".equals(event.Data)){
+            return query.qryAllTypes();
+        }
+    }
+
+
+    private Connection getConn() {
+        try {
+            Class.forName(driver);
+            if (conn == null)
+                conn = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            close();
+        }
+        return conn;
+    }
+
+    private void close() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Map> qryAllTypes() {
+        String sql = "select * from storage_type";
+        Connection conn = getConn();
+        Statement stmt = null;
+        ResultSet ret = null;
+        List result = new ArrayList();
+        String password = null;
+        try {
+            stmt = conn.createStatement();
+            ret = stmt.executeQuery(sql);
+            while (ret.next()) {
+                Map map = new HashMap();
+                map.put("typeid",ret.getString("typeid"));
+                map.put("typename",ret.getString("typename"));
+                map.put("parenttypeid",ret.getString("parenttypeid"));
+                map.put("typecomment",ret.getString("typecomment"));
+                result.add(map);
+            }
+            ret.close();
+            stmt.close();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+}
---
+++ kubeleess/src/test/java/QueryTest.java
@@ -0,0 +1,17 @@
+import kubeless.io.Query;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class QueryTest {
+
+    @Test
+    public void testQryAllTypes(){
+        Query query = new Query();
+        List<Map> list = query.qryAllTypes();
+        System.out.println(list.get(0));
+        Assert.assertNotNull(list.get(0));
+    }
+}
